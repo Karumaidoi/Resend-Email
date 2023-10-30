b56986bec6652502933e8acb3366be15c3cabdc4
Karumaidoi <mainahmwangi12@gmail.com>
2023-10-30 12:34:23 +0300

    code refactor

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 
+// Setting up our Route
 const emailRouter = require("./router/emailRouter");
 
 const app = express();
 
+// Configuring our app
 app.use(morgan("dev"));
 app.use(express.json());
 
+// Email Route
 app.use("/api/v1/email", emailRouter);
 
 module.exports = app;
